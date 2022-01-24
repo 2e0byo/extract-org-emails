28b3bd2f339d8dba8ee63cd26754e90ca2226401
John Maximilian <2e0byo@gmail.com>
2022-01-24 18:25:09 +0000

    feat: find slackers.

--- extract_org_emails.py
+++ extract_org_emails.py
@@ -1,5 +1,6 @@
 from itertools import chain
 from pathlib import Path
+from subprocess import run
 from typing import Iterable, Optional
 
 import click
@@ -8,7 +9,7 @@ from orgparse.extra import Table
 from orgparse.node import OrgNode
 
 
-def find_module(root, title: str, subheading: str = None):
+def find_module(root: OrgNode, title: str, subheading: str = None):
     title = title.lower()
     candidates = (x for x in root[1:] if x.heading.lower() == title)
     if subheading:
@@ -39,15 +40,99 @@ def process_module(root, title: str, subheading: str) -> Iterable[str]:
     return chain.from_iterable(extract_emails(t) for t in tables)
 
 
-@click.command()
+def find_registers(root: OrgNode):
+    return [make_table(x) for x in root[1:] if x.heading.lower().strip() == "students"]
+
+
+def get_seminars(row: dict):
+    seminars = []
+    for k in row.keys():
+        try:
+            int(k)
+            seminars.append(k)
+        except (TypeError, ValueError):
+            pass
+    return seminars
+
+
+def get_missed(row: dict, seminars: list, skip: int):
+    return len([1 for k in seminars if not row[k].strip() and int(k) > skip])
+
+
+def get_slackers(register: Table, skip: int, thresh: int = 2):
+    dicts = register.as_dicts
+    first = next(iter(dicts))
+    seminars = get_seminars(first)
+    return (
+        row | dict(Missed=missed, Emailed=has_emailed(row["Email"]))
+        for row in chain((first,), dicts)
+        if not row[seminars[-1]].strip()
+        if (missed := get_missed(row, seminars, skip)) >= thresh
+    )
+
+
+def get_all_slackers(root: OrgNode, skip: int, thresh: int = 2):
+    return chain.from_iterable(
+        get_slackers(x, skip, thresh) for x in find_registers(root)
+    )
+
+
+def has_emailed(email: str) -> str:
+    """Find out if the student emailed me in the last two weeks."""
+    querystr = f"f:{email} date:14d..now"
+    out = run(
+        ["mu", "find", querystr, "-f", "s"], capture_output=True, encoding="utf8"
+    ).stdout.splitlines()
+    return f"{out[0]} ({len(out)})" if out else ""
+
+
+def format_table(rows: list[dict], keys: list = None):
+    if not isinstance(rows, list):
+        rows = list(rows)
+
+    keys = keys or list(rows[0].keys())
+    rows = list(rows)
+    widths = {k: max(chain((len(str(r[k])) for r in rows), [len(k)])) for k in keys}
+    header = [
+        "|" + "|".join(f" {k:<{widths[k]}} " for k in keys) + "|",
+        "|" + "|".join("-" * (widths[k] + 2) for k in keys) + "|",
+    ]
+    lines = ["|" + "|".join(f" {r[k]:<{widths[k]}} " for k in keys) + "|" for r in rows]
+    return "\n".join(chain(header, lines))
+
+
+common = {}
+
+
+@click.group()
 @click.argument("path")
 @click.argument("module")
 @click.option("--subheading", help="Subheading to narrow search.")
-def main(path: str, module: str, subheading: Optional[str]):
-    root = load(Path(path).expanduser().resolve())
-    emails = process_module(root, module, subheading)
+def cli(path: str, module: str, subheading: Optional[str]):
+    common["root"] = load(Path(path).expanduser().resolve())
+    common["module"] = module
+    common["subheading"] = subheading
+
+
+@cli.command()
+def emails():
+    emails = process_module(common["root"], common["module"], common["subheading"])
     print("Bcc:", ", ".join(emails))
 
 
+@cli.command()
+@click.argument("skip")
+@click.option("--thresh", help="Threshold.")
+@click.option("--bcc/--no-bcc", help="Generate bcc", default=False)
+def slackers(skip: str, thresh: str = "2", bcc: bool = None):
+    thresh = thresh or 0
+    module = find_module(common["root"], common["module"], common["subheading"])
+    slackers = get_all_slackers(module, int(skip), int(thresh))
+    if not bcc:
+        print(format_table(slackers, ("Name", "Surname", "Email", "Missed", "Emailed")))
+    else:
+        print("Bcc: " + ",".join(x["Email"] for x in slackers))
+
+
 if __name__ == "__main__":
-    main()
+    cli()
